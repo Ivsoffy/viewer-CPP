45ce4b68ad1059dec233d40d3ed78abd9dcc0a52
Errok Elenor <errokele@student.21-school.ru>
2023-12-26 19:34:23 +0700

    uTochki2

--- src/3DViewer/Model.h
+++ src/3DViewer/Model.h
@@ -2,3 +2,7 @@
 #define MODEL_H
 
 #endif // MODEL_H
+class Model
+    {
+        private: int pass;
+    };
--- src/3DViewer/dto.cpp
+++ src/3DViewer/dto.cpp
@@ -2,5 +2,10 @@
 
 DTO::DTO()
 {
+    private:
+        double x_;
+        double y_;
+        double z_;
 
+        double rotate
 }
